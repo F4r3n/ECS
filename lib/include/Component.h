59760b3cf62cb60c48bf9ef9120ac3a1075cf441
F4r3n <guillaume_k@hotmail.fr>
2017-11-22 15:06:49 +0100

    serialization

--- lib/include/Component.h
+++ lib/include/Component.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <memory>
 #include <typeinfo> //for 'typeid' to work
+#include "Serializer.hpp"
 class ComponentManager;
 
 #include <cstddef>
@@ -10,6 +11,7 @@ public:
     BaseComponent();
     virtual ~BaseComponent();
     const std::string& getName() const {return _name;}
+    virtual void serialize(Serializer &serializer) {} 
 protected:
     std::string _name;
 
@@ -21,14 +23,12 @@ template <class T> class Component : public BaseComponent {
 
 public:
     Component() {
-       
     }
 
     static size_t id() {
         static size_t i = family_counter++;
         return i;
     }
-
     virtual ~Component() {
     }
     friend class ComponentManager;
